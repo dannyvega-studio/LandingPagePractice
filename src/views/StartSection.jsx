52af532d7a9b0b5d5c5fc91e8925fdd3f121c328
Danny Vega <67448064+dannyvega-studio@users.noreply.github.com>
2021-07-13 10:11:30 -0600

    Fixed Ximena's Name

--- src/views/StartSection.jsx
+++ src/views/StartSection.jsx
@@ -24,8 +24,8 @@ function StartSection(props) {
                 </Col>
                 <Row className="mt-4 mb-4">
                     <Col lg={4} md={12} sm={12}>
-                        <h3 class="mt-4">Ximena Cardenas</h3>
-                        <img src={profileXC} alt="Ivan Cobos" class="profile-img" />
+                        <h3 class="mt-4">Ximena Cadena</h3>
+                        <img src={profileXC} alt="Ximena Cadena" class="profile-img" />
                         <h2 class="mt-4">Coach</h2>
                         <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </Col>
